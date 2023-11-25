af4b3306b3d1d3b86eaf2d04ef0097ae9a1ab42b
Mohd_Labib <karibschool.web@gmail.com>
2023-11-26 00:43:11 +0700

    update

--- src/main/java/labib/Main.java
+++ src/main/java/labib/Main.java
@@ -2,11 +2,6 @@ package labib;
 
 public class Main {
     public static void main(String[] args) {
-        // java.awt.EventQueue.invokeLater(new Runnable() {
-        //     public void run() {
-        //         new Frame();
-        //     }
-        // });
         new Frame();
     }
 }
